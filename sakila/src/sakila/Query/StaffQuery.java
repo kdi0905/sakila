6c4ad31038d9e4c0a1bb939f9d05478f1e027fc8
GD10 <GD10@DESKTOP-ERIRT25>
2020-10-22 17:35:00 +0900

    query staff_id-> email

--- sakila/src/sakila/Query/StaffQuery.java
+++ sakila/src/sakila/Query/StaffQuery.java
@@ -3,5 +3,5 @@ package sakila.Query;
 public class StaffQuery {
 	//로그인 확인
 	public final static String SELECT_STAFF_BY_KEY 
-	="SELECT staff_id, username FROM staff WHERE staff_id=? AND password=PASSWORD(?)";
+	="SELECT staff_id, username FROM staff WHERE email=? AND password=PASSWORD(?)";
 }
